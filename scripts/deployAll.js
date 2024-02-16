678ac9803a0d32dc0d5e80340a8d9767d39ad675
Haipls <bohdan.h@b-ee.io>
2024-02-16 22:11:34 +0200

    add configuration & deploy on blast sepolia network

--- scripts/deployAll.js
+++ scripts/deployAll.js
@@ -2,10 +2,10 @@ const execSync = require('child_process').execSync;
 
 const network = process.argv[2];
 
-execSync(`cd src/core && npx hardhat run --network ${network} scripts/deploy.js`, {stdio: 'inherit'});
+execSync(`cd src/core && npx hardhat run --network ${network} scripts/deploy.js`, { stdio: 'inherit' });
 
-execSync(`cd src/plugin && npx hardhat run --network ${network} scripts/deploy.js`, {stdio: 'inherit'});
+execSync(`cd src/plugin && npx hardhat run --network ${network} scripts/deploy.js`, { stdio: 'inherit' });
 
-execSync(`cd src/periphery && npx hardhat run --network ${network} scripts/deploy.js`, {stdio: 'inherit'});
+execSync(`cd src/periphery && npx hardhat run --network ${network} scripts/deploy.js`, { stdio: 'inherit' });
 
-execSync(`cd src/farming && npx hardhat run --network ${network} scripts/deploy.js`, {stdio: 'inherit'});
+execSync(`cd src/farming && npx hardhat run --network ${network} scripts/deploy.js`, { stdio: 'inherit' });
--- src/farming/scripts/deploy.js
+++ src/farming/scripts/deploy.js
@@ -1,41 +1,38 @@
-const hre = require('hardhat')
-const fs = require('fs')
-const path = require('path')
-const BasePluginV1FactoryComplied = require('@cryptoalgebra/integral-base-plugin/artifacts/contracts/BasePluginV1Factory.sol/BasePluginV1Factory.json');
+const hre = require('hardhat');
+const fs = require('fs');
+const path = require('path');
+const BasePluginV1FactoryComplied = require('../../plugin/artifacts/contracts/BasePluginV1Factory.sol/BasePluginV1Factory.json');
 
 async function main() {
-  const deployDataPath = path.resolve(__dirname, '../../../deploys.json')
-  const deploysData = JSON.parse(fs.readFileSync(deployDataPath, 'utf8'))
+  const deployDataPath = path.resolve(__dirname, '../../../deploys.json');
+  const deploysData = JSON.parse(fs.readFileSync(deployDataPath, 'utf8'));
 
-  const AlgebraEternalFarmingFactory = await hre.ethers.getContractFactory('AlgebraEternalFarming')
-  const AlgebraEternalFarming = await AlgebraEternalFarmingFactory.deploy(deploysData.poolDeployer, deploysData.nonfungiblePositionManager)
+  const AlgebraEternalFarmingFactory = await hre.ethers.getContractFactory('AlgebraEternalFarming');
+  const AlgebraEternalFarming = await AlgebraEternalFarmingFactory.deploy(deploysData.poolDeployer, deploysData.nonfungiblePositionManager);
 
   deploysData.eternal = AlgebraEternalFarming.target;
 
-  await AlgebraEternalFarming.waitForDeployment()
-  console.log('AlgebraEternalFarming deployed to:', AlgebraEternalFarming.target)
+  await AlgebraEternalFarming.waitForDeployment();
+  console.log('AlgebraEternalFarming deployed to:', AlgebraEternalFarming.target);
 
-  const FarmingCenterFactory = await hre.ethers.getContractFactory('FarmingCenter')
-  const FarmingCenter = await FarmingCenterFactory.deploy(AlgebraEternalFarming.target, deploysData.nonfungiblePositionManager)
+  const FarmingCenterFactory = await hre.ethers.getContractFactory('FarmingCenter');
+  const FarmingCenter = await FarmingCenterFactory.deploy(AlgebraEternalFarming.target, deploysData.nonfungiblePositionManager);
 
   deploysData.fc = FarmingCenter.target;
 
-  await FarmingCenter.waitForDeployment()
-  console.log('FarmingCenter deployed to:', FarmingCenter.target)
+  await FarmingCenter.waitForDeployment();
+  console.log('FarmingCenter deployed to:', FarmingCenter.target);
 
-  await AlgebraEternalFarming.setFarmingCenterAddress(FarmingCenter.target)
-  console.log('Updated farming center address in eternal(incentive) farming')
+  await AlgebraEternalFarming.setFarmingCenterAddress(FarmingCenter.target);
+  console.log('Updated farming center address in eternal(incentive) farming');
 
-  const pluginFactory = await hre.ethers.getContractAt(BasePluginV1FactoryComplied.abi, deploysData.BasePluginV1Factory)
+  const pluginFactory = await hre.ethers.getContractAt(BasePluginV1FactoryComplied.abi, deploysData.BasePluginV1Factory);
 
-  await pluginFactory.setFarmingAddress(FarmingCenter.target)
-  console.log('Updated farming center address in plugin factory')
+  await pluginFactory.setFarmingAddress(FarmingCenter.target);
+  console.log('Updated farming center address in plugin factory');
 
-  const posManager = await hre.ethers.getContractAt(
-    'INonfungiblePositionManager',
-    deploysData.nonfungiblePositionManager
-  )
-  await posManager.setFarmingCenter(FarmingCenter.target)
+  const posManager = await hre.ethers.getContractAt('INonfungiblePositionManager', deploysData.nonfungiblePositionManager);
+  await posManager.setFarmingCenter(FarmingCenter.target);
 
   fs.writeFileSync(deployDataPath, JSON.stringify(deploysData), 'utf-8');
 }
@@ -45,6 +42,6 @@ async function main() {
 main()
   .then(() => process.exit(0))
   .catch((error) => {
-    console.error(error)
-    process.exit(1)
-  })
+    console.error(error);
+    process.exit(1);
+  });
