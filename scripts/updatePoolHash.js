e1724c9de912fd41e61231b135056bdf70277f8c
Aegas Dev <support@aegas.io>
2024-04-01 17:16:45 +0300

    fix updatePoolHash script

--- scripts/updatePoolHash.js
+++ scripts/updatePoolHash.js
@@ -1,26 +1,23 @@
 const fs = require('fs');
 const path = require('path');
-const {keccak256} = require('@ethersproject/solidity');
+const { keccak256 } = require('@ethersproject/solidity');
 const execSync = require('child_process').execSync;
 
 function updateHash(pathToPool, files) {
-    const contract = JSON.parse(fs.readFileSync(path.resolve(__dirname, pathToPool), 'utf8'));
+  const contract = JSON.parse(fs.readFileSync(path.resolve(__dirname, pathToPool), 'utf8'));
 
-    const COMPUTED_INIT_CODE_HASH = keccak256(['bytes'], [`${contract["bytecode"]}`])
+  const COMPUTED_INIT_CODE_HASH = keccak256(['bytes'], [`${contract['bytecode']}`]);
 
-    for (let file of files) {
-        let getAddress = fs.readFileSync(path.resolve(__dirname, file), 'utf8');
-        getAddress = getAddress.replace(/POOL_INIT_CODE_HASH = [^;]+;/, `POOL_INIT_CODE_HASH = ${COMPUTED_INIT_CODE_HASH};`)
-        fs.writeFileSync(path.resolve(__dirname, file), getAddress, 'utf-8');
+  for (let file of files) {
+    let getAddress = fs.readFileSync(path.resolve(__dirname, file), 'utf8');
+    getAddress = getAddress.replace(/POOL_INIT_CODE_HASH = [^;]+;/, `POOL_INIT_CODE_HASH = ${COMPUTED_INIT_CODE_HASH};`);
+    fs.writeFileSync(path.resolve(__dirname, file), getAddress, 'utf-8');
 
-        execSync('git add "' + path.resolve(__dirname, file) + '"');
-    }
+    execSync('git add "' + path.resolve(__dirname, file) + '"');
+  }
 }
 
-updateHash(
-    '../src/core/artifacts/contracts/AlgebraPool.sol/AlgebraPool.json', 
-    [
-        '../src/periphery/contracts/libraries/PoolAddress.sol',
-        '../src/core/contracts/AlgebraFactory.sol',
-    ]
-);
+updateHash('../src/core/artifacts/contracts/AlgebraPool.sol/AlgebraPool.json', [
+  '../src/periphery/contracts/libraries/PoolAddress.sol',
+  '../src/core/contracts/AlgebraFactoryUpgradeable.sol',
+]);
